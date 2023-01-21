c5fe92482b5dd016a0e1d5cd262565eb45ac7154
Daniel Havlik <dh@gocept.com>
2023-01-21 23:44:48 +0100

    add fancy filters to admin

--- einwise/einweisungen/admin.py
+++ einwise/einweisungen/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from .models import Area, Member, Einweisable, Einweisung
+from admin_searchable_dropdown.filters import AutocompleteFilterFactory
 
 
 class EinweisungAdmin(admin.ModelAdmin):
     list_display = ('member', 'level',  'einweisable', 'instructor', 
                     'issue_date')
-    list_filter = ['level', 'member', 'einweisable']
+
+    autocomplete_fields = ['member', 'einweisable']
+    list_filter = ['level',
+        AutocompleteFilterFactory('Mitglied', 'member'),
+        AutocompleteFilterFactory('Einweisable', 'einweisable')]
+    search_fields = ['member__name', 'einweisable__name']
+
     
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name in ("member", "instructor"):
@@ -18,12 +25,14 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ('member_id', 'name', 'is_active')
     list_filter = ['is_active']
     ordering = ('member_id',)
+    search_fields = ['name']
 
 
 class EinweisableAdmin(admin.ModelAdmin):
     list_display = ('name', 'area', 'etype')
     list_filter = ('area',)
     ordering = ('area', 'etype', 'name')
+    search_fields = ['name']
 
 
 admin.site.register(Member, MemberAdmin)
